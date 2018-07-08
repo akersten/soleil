64dca8cc00365cda39e4c77614b5537cc531e735
Alex Kersten <alex@kersten.email>
2018-07-08 01:31:17 -0500

    add royalty titles

---
+++ src/ts/core/character/flair/Nobility.ts
@@ -0,0 +1,25 @@
+export enum ENobility {
+    PEASANT,
+    COMMONER,
+    GENTLEMAN,
+    SQUIRE,
+    KNIGHT,
+
+    BARONET,
+    BARON,
+    VISCOUNT,
+    COUNT,
+    EARL,
+
+    MARQUESS,
+    VIZIER,
+    GRAND_VIZIER,
+    VICEROY,
+    DUKE,
+
+    ARCHDUKE,
+    PRINCE,
+    KING,
+    EMPEROR,
+    IMPERATOR,
+}
